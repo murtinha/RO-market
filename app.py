58b04f0fb3a289d920b71fecdc9a17fdac9edc71
Eric Murta <ericmurta@hotmail.com>
2018-06-12 00:18:05 -0300

    Add database and add-item route

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
 from flask import Flask, jsonify, request
-from crawler import get_lowest_price
+from flask_sqlalchemy import SQLAlchemy
+from crawler import get_item
+import os
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+db = SQLAlchemy(app)
+
+class Item(db.Model):
+    __tablename__ = 'items'
+
+    id = db.Column(db.Integer, primary_key=True)
+    item_id = db.Column(db.Integer, unique=True)
+    name = db.Column(db.String(100))
+    price = db.Column(db.String(100))
+
+    def __init__(self, item_id, name, price):
+        self.item_id = item_id
+        self.name = name
+        self.price = price
+
+    def __repr__(self):
+        return '<Item %r>' % (self.name)
 
 @app.route('/')
 def hello_world():
@@ -9,9 +29,20 @@ def hello_world():
 
 @app.route('/lowest-price')
 def lowest_price():
-    id = request.args.get('id')
-    price = get_lowest_price(id)
-    return jsonify(price)
+    item_id = request.args.get('id')
+    item = get_item(item_id)
+    return jsonify(dict(price = item.get('price', '')))
+
+@app.route('/add-item', methods=['POST'])
+def add_item():
+    item_id = request.args.get('id')
+    item = get_item(item_id)
+    price = item.get('price', '')
+    name = item.get('name', '')
+    dbcreate = Item(item_id, name, price) 
+    db.session.add(dbcreate)
+    db.session.commit()
+    return jsonify(dict(message = '%s was added to database'% name))
 
 if __name__ == '__main__':
     app.run(debug=True)
--- crawler.py
+++ crawler.py
@@ -2,7 +2,7 @@ import requests
 import json
 from bs4 import BeautifulSoup
 
-def get_lowest_price(item_id):
+def get_item(item_id):
     headers = {
         'user-agent': 'my-app/0.0.1',
         'cookie': '__cfduid=dd2bc98b37a58f03b9074d7a4fd137ea21527513404; _ga=GA1.2.279426244.1527513406; fluxSessionData=6df671d7359a7e03c118711b9ba030d5; _gid=GA1.2.1024025882.1528670382; cookiescriptaccept=visit',
